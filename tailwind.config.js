ffa0e422548e5dd91d445d9aca4040ff2e907b45
Richard Jongenburger <richard.jongenburger@gmail.com>
2022-09-26 18:19:31 +0200

    chore: setup project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: {
+        100: "#9ed7fb",
+        200: "#8ed0fb",
+        300: "#7dc9fa",
+        400: "#6dc3fa",
+        500: "#5DBCF9",
+        600: "#54a9e0",
+        700: "#4a96c7",
+        800: "#4184ae",
+      },
+      secondary: {
+        100: "#9ed7fb",
+        200: "#8ed0fb",
+        300: "#7dc9fa",
+        400: "#6dc3fa",
+        500: "#191919",
+        600: "#54a9e0",
+        700: "#4a96c7",
+        800: "#4184ae",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
